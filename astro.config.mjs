64847e8d05ef6ed3062b3ec7bb73b370fb4252e8
eddndev <contacto@eddndev.com>
2025-12-31 13:01:48 -0600

    feat(seo): add sitemap, robots.txt and improve meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://eddn.dev',
   vite: {
     plugins: [tailwindcss()],
   },
+  integrations: [sitemap()],
 });
